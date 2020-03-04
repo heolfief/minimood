6e9ea1d054e8acb267afeead57d1c2c73576f76f
heolfief <heol.fief@gmail.com>
2020-03-04 15:07:37 +0100

    Change HMI Tim7 IT frequency according to new core freq

--- Software/Firmware/Core/Src/main.c
+++ Software/Firmware/Core/Src/main.c
@@ -467,7 +467,7 @@ static void MX_TIM7_Init(void)
 
   /* USER CODE END TIM7_Init 1 */
   htim7.Instance = TIM7;
-  htim7.Init.Prescaler = 8400;
+  htim7.Init.Prescaler = 9000;
   htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim7.Init.Period = 199;
   htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
